5fb9976cd6d8755e6f2502300f8bb947c71b0edc
Fábio Amaral <fabio.pamaral@outlook.com>
2025-08-05 15:06:29 -0300

    chore: supabase config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,25 @@
 // src/app/layout.tsx
+import Link from "next/link";
 import "./globals.css";
 import { ReactNode } from "react";
 
 export const metadata = {
   title: "E-commerce",
-  description: "Sistema completo de e-commerce com Next.js",
+  description: "e-commerce system with Next.js",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
       <body className="bg-gray-50 text-gray-900">
-        <header className="p-4 shadow-md">Minha Loja</header>
+        <header className="p-4 shadow-md flex justify-between">
+          <div>Minha Loja</div>
+          <nav>
+            <Link href="/login" className="text-blue-600 hover:underline">
+              Entrar
+            </Link>
+          </nav>
+        </header>
 
         <main className="p-4">{children}</main>
 
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+if (!supabaseAnonKey || !supabaseUrl)
+  throw new Error("supabase env variables not defined");
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
